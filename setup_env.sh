ba7f490040ff1654d90a7b4067f50d007f28565e
aceaton <aceaton@princeton.edu>
2023-11-25 15:46:15 -0500

    put help files in there

--- setup_env.sh
+++ setup_env.sh
@@ -6,7 +6,8 @@ sudo apt update && sudo apt dist-upgrade
 sudo apt install cmake libglib2.0-dev libgcrypt20-dev flex yacc bison byacc \
   libpcap-dev qtbase5-dev libssh-dev libsystemd-dev qtmultimedia5-dev \
   libqt5svg5-dev qttools5-dev
-sudo apt-get install libc-ares-dev
+sudo apt-get install libc-ares-dev 
+# sudo apt-get install qconf qmake6 qmake6-bin
 cmake .
 make
 sudo make install
