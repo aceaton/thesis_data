1211cad071cd35e0eeef4d6e7348254a1952e8bc
aceaton <aceaton@princeton.edu>
2023-11-26 13:05:48 -0500

    set up multi website testing wroflow

--- record.py
+++ record.py
@@ -1,3 +1,6 @@
+# WHEN DOING THIS ON REMOTE, TURN OFF CHROMEDRIVER LOCATION, and INTERFACE
+
+
 import subprocess
 import time
 from selenium import webdriver
@@ -8,13 +11,14 @@ import os
 
 from data_specs import *
 
-current_directory = os.get_cwd()
+current_directory = os.getcwd()
 relative_path = 'captured_data'
 directory_path = os.path.join(current_directory,relative_path)
 
 # Function to start TShark capture
 def start_tshark_capture(port, ip, filename):
     # filter_expression = f"tcp port {port}" # and dst {ip}"
+    # inter = "en0"
     inter = "eno1"
     filter_expression = f"tcp and dst {ip}"
 
@@ -28,18 +32,28 @@ def start_tshark_capture(port, ip, filename):
 
 # Function to open a website using Selenium
 def open_website(port,url,t):
+    status = 1
     # chromedriver_install_loc = "/Users/annaeaton/Downloads/chromedriver-mac-x64-119/chromedriver"
-    chromedriver_install_loc = "/users/aceaton/chromedriver"
+    # chromedriver_install_loc = "/users/aceaton/chromedriver"
     # driver = webdriver.Chrome()  # Make sure you have the ChromeDriver executable in your PATH
     options = webdriver.ChromeOptions()
-    options.add_argument(f"--remote-debugging-port={port}")
+    # options.add_argument(f"--remote-debugging-port={port}")
     options.add_argument("--headless")  # Run in headless mode
     options.add_argument("--disable-gpu")  # Disable GPU acceleration
-    driver = webdriver.Chrome(options=options)#,executable_path=chromedriver_install_loc) 
-    driver.get(url)
-    print(f"got {url}")
-    time.sleep(t)  # Wait for 10 seconds (adjust as needed)
+    driver = webdriver.Chrome(options=options) 
+    # driver = webdriver.Chrome(options=options,executable_path=chromedriver_install_loc) 
+    print(f"tried {url}")
+    try:
+        driver.get(url)
+        print(f"got {url}")
+        # Wait for 10 seconds (adjust as needed)
+        time.sleep(t)
+    except:
+        print(f"failed to get {url}")
+        status = 0
+      
     driver.quit()
+    return status
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -54,9 +68,11 @@ def get_local_ip():
     return local_ip
 
 
-def capture_site(site, number, time):
+def capture_site(site, number, capture_time):
     # port_to_capture = 80  # Replace with the desired port
     port_to_capture = 8080  # Specify the desired port for TShark and Selenium
+
+    wait_time = 5
 
     # Get local IP address
     local_ip = get_local_ip()
@@ -66,27 +82,35 @@ def capture_site(site, number, time):
         # If it doesn't exist, create the directory
         os.makedirs(site_path)
         print(f"Directory '{site_path}' created.")
-    filename = relative_path+"/"+str(site)+"/"+str(site)+"_"+str(time)+"_"+str(number)+".pcap"
+    filename = relative_path+"/"+str(site)+"/"+str(site)+"_"+str(capture_time)+"_"+str(number)+".pcap"
 
     # Start TShark capture in the background
     start_tshark_capture(port_to_capture,local_ip,filename)
 
+    print("started_capturing")
     # Open the website in Selenium
-    open_website(port_to_capture,site,time)
-
-    # Wait for some time (adjust as needed)
-    time.sleep(time)
+    status = open_website(port_to_capture,"https://www."+site,capture_time)
 
     # Stop TShark capture (you might need to find the correct process ID)
     subprocess.run(["pkill", "tshark"])
 
+    # Wait for some time (adjust as needed)
+    time.sleep(5)
+    return status
+
 
 def test():
+    failures = []
     # specs from data_specs
-    for site in websites[:3]:
-        print(site)
-        for i in range(samples_per_site):
-            capture_site(site,i,capture_time)
+    for i in range(samples_per_site):
+        for site in websites:
+            print(site)
             print(i)
+            status = capture_site(site,i,capture_time)
+            if (status == 0):
+                failures.append(site)
+
+    print("Failures:")
+    print(failures)
 
 test()
